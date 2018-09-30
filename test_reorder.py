120078b6d205a162bb00ef2dc60cd30625bfcb9e
alan <alan.so@sky.uk>
2018-09-30 11:33:20 +0100

    Squashed commit of the following:
    commit 346b183ed19fef6bc6680b4e6b9b7f9320da433f
    Author: alan <alan.so@sky.uk>
    Date:   Sun Sep 30 11:27:06 2018 +0100
    
        simple code layout for single file utility

--- test_reorder.py
+++ test_reorder.py
@@ -1,17 +1,17 @@
 from unittest import TestCase, mock
-from reorder import Economist
+from reorder import Economist, get_parser, set_preferred_order
 
 
 class TestEconomist(TestCase):
     def setUp(self):
         self.maxDiff = None
-        self.economist = Economist()
-        self.economist.download_folder = 'New_Issue'
+        self.economist = Economist('New_Issue')
 
-    def test_get_source(self):
+    def test_get_parser(self):
         with mock.patch('sys.argv', ['', 'Latest_Issue']):
-            self.economist.get_source()
-            self.assertEqual('Latest_Issue', self.economist.download_folder)
+            parser = get_parser()
+            args = vars(parser.parse_args())
+            self.assertEqual('Latest_Issue', args['download_folder'])
 
     def test_audio_files(self):
         # Return a list of only  mp3 file names
@@ -26,18 +26,16 @@ class TestEconomist(TestCase):
 
         with mock.patch('reorder.os.listdir', return_value=mp3s):
             with mock.patch('reorder.os.path.exists', return_value=True) as MockHelper:
-                economist = Economist()
-                economist.download_folder = 'New_Issue'
+                economist = Economist('New_Issue')
                 economist.find_audio_files()
                 self.assertEqual(economist.file_list, expected)
                 MockHelper.assert_called_with('New_Issue')
 
     def test_audio_files_directory_not_found(self):
         with mock.patch('reorder.os.path.exists', return_value=False) as MockHelper:
-            economist = Economist()
-            economist.download_folder = 'New_Issue'
+            economist = Economist('No_such_folder')
             self.assertRaises(FileNotFoundError, economist.find_audio_files)
-            MockHelper.assert_called_with('New_Issue')
+            MockHelper.assert_called_with('No_such_folder')
 
     def test_set_preferred_order(self):
         # Generate a list of source/destination file paths
@@ -53,3 +51,16 @@ class TestEconomist(TestCase):
                 self.economist.set_preferred_order()
                 self.assertEqual(self.economist.source_destination_paths, expected)
                 MockHelper.assert_called_with('/Users/alan/workspace/economist/New_Issue')
+
+    def test_set_playlist_order(self):
+        # Generate a list of destination/source file paths
+        mp3s = ['001 Introduction.mp3',
+                '002 The world this week - Politics this week.mp3',
+                '078 Science and technology - Recycling.mp3']
+        expected = [('/new destination folder/New_Issue/01002 The world this week - Politics this week.mp3',
+                     '/origin folder/002 The world this week - Politics this  week.mp3'),
+                    ('/new destination folder/New_Issue/02078 Science and technology - Recycling.mp3',
+                     '/origin folder/078 Science and technology - Recycling.mp3')]
+
+        # actual = set_preferred_order('/origin folder', mp3s)
+        # self.assertEqual(actual, expected)
